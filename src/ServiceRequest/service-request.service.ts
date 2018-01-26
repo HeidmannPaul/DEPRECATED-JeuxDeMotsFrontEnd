2e9252add6c39c0de037787f5fa190aac492e1df
HeidmannPaul <paulheid@hotmail.fr>
2018-01-26 19:47:36 +0100

    Add recherche avec relations

--- src/ServiceRequest/service-request.service.ts
+++ src/ServiceRequest/service-request.service.ts
@@ -22,5 +22,18 @@ export class ServiceRequestService {
     let observable = this.http.get(url).map((res:Response)=> res.json());
     return observable;
   }
+  getValueByIdRelations(paramValue: any,paramRel:any): Observable<any> {
+    let url = "http://localhost:8888/" + paramValue+"/Rel/"+paramRel;
+    var headers = new Headers();
+    let observable = this.http.get(url);//.map((res:Response)=> res.toString());
+    return observable;
+  }
+
+  getRelationComplete(param: any): Observable<any> {
+    let url = "http://localhost:8888/Rel/" + param;
+    var headers = new Headers();
+    let observable = this.http.get(url).map((res:Response)=> res.json());
+    return observable;
+  }
 
 }
